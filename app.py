018c98d87d4aa2bb66107d824c5a6ab659cd7b9e
oussamaelfig <oelfigha2002@gmail.com>
2023-02-17 07:59:22 -0500

    add routes

--- app.py
+++ app.py
@@ -62,5 +62,94 @@ def article(identifier):
     return render_template('article.html', article=article)
 
 
+@app.route('/admin')
+def admin():
+    # Connect to the database
+    conn = sqlite3.connect('db/infoArticle.db')
+    c = conn.cursor()
+
+    # Retrieve all articles
+    c.execute("SELECT identifiant, titre, date_publication FROM article")
+    articles = c.fetchall()
+
+    # Close the database connection
+    conn.close()
+
+    return render_template('admin.html', articles=articles)
+
+
+@app.route('/admin-editer/<identifier>', methods=['GET', 'POST'])
+def admin_edit(identifier):
+    # Connect to the database
+    conn = sqlite3.connect('db/infoArticle.db')
+    c = conn.cursor()
+
+    # Retrieve the article with the specified identifier
+    c.execute("SELECT * FROM article WHERE identifiant=?", (identifier,))
+    article = c.fetchone()
+
+    # If no article was found, return a 404 error page
+    if article is None:
+        return render_template('404.html'), 404
+
+    if request.method == 'POST':
+        # Retrieve the form data
+        titre = request.form['titre']
+        paragraphe = request.form['paragraphe']
+
+        # Update the article in the database
+        c.execute("UPDATE article SET titre=?, paragraphe=? WHERE identifiant=?",
+                  (titre, paragraphe, identifier))
+        conn.commit()
+
+        # Close the database connection
+        conn.close()
+
+        # Redirect to the article page
+        return redirect(url_for('article', identifier=identifier))
+
+    # Close the database connection
+    conn.close()
+
+    return render_template('admin_edit.html', article=article)
+
+
+@app.route('/admin-nouveau', methods=['GET', 'POST'])
+def admin_new():
+    if request.method == 'POST':
+        # Connect to the database
+        conn = sqlite3.connect('db/infoArticle.db')
+        c = conn.cursor()
+
+        # Retrieve the form data
+        titre = request.form['titre']
+        paragraphe = request.form['paragraphe']
+        date_publication = request.form['date_publication']
+
+        # Validate the form data
+        if not titre or not paragraphe or not date_publication:
+            error = 'All fields are required'
+            return render_template('admin_new.html', titre=titre, paragraphe=paragraphe,
+                                   date_publication=date_publication, error=error)
+
+        try:
+            datetime.strptime(date_publication, '%Y-%m-%d')
+        except ValueError:
+            error = 'Invalid date format, please use YYYY-MM-DD'
+            return render_template('admin_new.html', titre=titre, paragraphe=paragraphe,
+                                   date_publication=date_publication, error=error)
+
+        # Insert the new article into the database
+        c.execute("INSERT INTO article (titre, paragraphe, date_publication) VALUES (?, ?, ?)",
+                  (titre, paragraphe, date_publication))
+        conn.commit()
+
+        # Close the database connection
+        conn.close()
+
+        # Redirect to the admin page
+        return redirect(url_for('admin'))
+
+
 if __name__ == '__main__':
     app.run(debug=True)
